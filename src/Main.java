e06e3cd8f507e2c7ec753f549f2761ffc5a59421
elijahinamarga <elijahcek@gmail.com>
2025-03-13 15:07:05 -0700

    Basic application implementation

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Submit");
+        button.setBounds(150, 200, 220, 50);
+        frame.add(button);
+        frame.setSize(500, 600);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
