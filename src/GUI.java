c8e996651c7cea328bfa6f9f691a600a5a42cf41
elijahinamarga <elijahcek@gmail.com>
2025-03-13 19:32:37 -0700

    Added output box

--- src/GUI.java
+++ src/GUI.java
@@ -4,22 +4,27 @@ public class GUI {
     final int APP_X_SIZE = 1000;
     final int APP_Y_SIZE = 1000;
     JFrame frame;
-    JButton btn;
-    JTextField text;
+    JButton submitButton;
+    JTextField filePath;
+    JTextField output;
 
     GUI() {
-       JFrame frame = new JFrame();
-       JButton button = new JButton("Submit");
-       JTextField text = new JTextField("Input file path...");
+       frame = new JFrame();
+       submitButton = new JButton("Submit");
+       filePath = new JTextField("Input file path...");
+       output = new JTextField("N/A");
 
-       text.setEditable(true);
-       text.setBounds(20, 20, 220, 50);
-       button.setBounds(20, 70, 220, 50);
+       filePath.setEditable(true);
+       filePath.setBounds(20, 20, 220, 50);
+       output.setEditable(false);
+       output.setBounds(APP_X_SIZE / 4, APP_Y_SIZE / 4, 500, 500);
+       submitButton.setBounds(20, 70, 220, 50);
 
        frame.setSize(APP_X_SIZE, APP_Y_SIZE);
        frame.setLayout(null);
        frame.setVisible(true);
-       frame.add(button);
-       frame.add(text);
+       frame.add(submitButton);
+       frame.add(filePath);
+       frame.add(output);
    }
 }
