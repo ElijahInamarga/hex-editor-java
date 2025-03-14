1225bac519a236f2741abf833ee504ed2e9f0925
elijahinamarga <elijahcek@gmail.com>
2025-03-13 17:39:59 -0700

    Refactored GUI class

--- src/GUI.java
+++ src/GUI.java
@@ -1,23 +1,25 @@
 import javax.swing.*;
-import java.io.*;
 
-public class Gui {
+public class GUI {
+    final int APP_X_SIZE = 1000;
+    final int APP_Y_SIZE = 1000;
     JFrame frame;
-    JButton button;
+    JButton btn;
     JTextField text;
 
-    Gui() {
+    GUI() {
        JFrame frame = new JFrame();
        JButton button = new JButton("Submit");
        JTextField text = new JTextField("Input file path...");
 
        text.setEditable(true);
-       text.setBounds(340, 200, 220, 50);
-       button.setBounds(340, 250, 220, 50);
-       frame.add(button);
-       frame.add(text);
-       frame.setSize(900, 700);
+       text.setBounds(20, 20, 220, 50);
+       button.setBounds(20, 70, 220, 50);
+
+       frame.setSize(APP_X_SIZE, APP_Y_SIZE);
        frame.setLayout(null);
        frame.setVisible(true);
+       frame.add(button);
+       frame.add(text);
    }
 }
