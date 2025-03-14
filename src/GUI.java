695138e378fcbe3137d4be8b7dd61e43e33bf504
elijahinamarga <elijahcek@gmail.com>
2025-03-13 20:11:47 -0700

    Refactored GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -22,13 +22,13 @@ public class GUI {
        output.setBounds(APP_X_SIZE / 4, APP_Y_SIZE / 4, 500, 500);
 
        submitButton.setBounds(20, 70, 220, 50);
-        submitButton.addActionListener(new ActionListener() {
+       submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 output.setText("This works!");
                 inputFilePath.setText("Input file path...");
             }
-        });
+       });
 
        frame.setSize(APP_X_SIZE, APP_Y_SIZE);
        frame.setLayout(null);
