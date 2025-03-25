aa61cbe4cc6d36740b5957097bcfe89b9f1ee12b
Christian <christianchang2023@gmail.com>
2025-03-25 14:27:50 -0700

    auto deletes the "insert file path here" text when clicking on text box

--- src/GUI.java
+++ src/GUI.java
@@ -37,13 +37,22 @@ public class GUI {
         inputPanel.add(findFileButton, BorderLayout.WEST);
 
         // Clicking on the file path input text box auto selects the whole input field
+//        inputFilePath.addFocusListener(new FocusAdapter() {
+//            @Override
+//            public void focusGained(FocusEvent e) {
+//                inputFilePath.setSelectionColor(Color.ORANGE);
+//                inputFilePath.selectAll();
+//            }
+//        });
+
+        //clicking on file path input text box auto deletes everything in the input field.
         inputFilePath.addFocusListener(new FocusAdapter() {
             @Override
             public void focusGained(FocusEvent e) {
-                inputFilePath.setSelectionColor(Color.ORANGE);
-                inputFilePath.selectAll();
-            }
-        });
+                // Clear text only if it hasn't already been cleared
+                if (!inputFilePath.getText().isEmpty()) {
+                    inputFilePath.setText("");
+                }
 
         frame.add(inputPanel, BorderLayout.NORTH);
         frame.add(outputPanel, BorderLayout.CENTER);
